1dca4ff3c5432c85c0ee696b142fe44350f0cf23
shoy <634330628@qq.com>
2015-05-11 17:16:45 +0800

    整理UI项目结构，添加常用组件项目

--- WebUI/Shoy.Ued/source/js/plugs/Jq-pseudo.js
+++ WebUI/Shoy.Ued/source/js/plugs/Jq-pseudo.js
@@ -1,4 +1,4 @@
-(function($){
+(function ($) {
 
     var patterns = {
         text: /^['"]?(.+?)["']?$/,
@@ -6,32 +6,31 @@
     };
 
     function clean(content) {
-        if(content && content.length) {
+        if (content && content.length) {
             var text = content.match(patterns.text)[1],
                 url = text.match(patterns.url);
-            return url ? '<img src="' + url[1] + '" />': text;
+            return url ? '<img src="' + url[1] + '" />' : text;
         }
     }
 
     function inject(prop, elem, content) {
-        if(prop != 'after') prop = 'before';
-        if(content = clean(elem.currentStyle[prop])) {
+        if (prop != 'after') prop = 'before';
+        if (content = clean(elem.currentStyle[prop])) {
             $(elem)[prop == 'before' ? 'prepend' : 'append'](
                 $(document.createElement('span')).addClass(prop).html(content)
             );
         }
     }
 
-    $.pseudo = function(elem) {
+    $.pseudo = function (elem) {
         inject('before', elem);
         inject('after', elem);
         elem.runtimeStyle.behavior = null;
     };
 
-    if(document.createStyleSheet) {
+    if (document.createStyleSheet) {
         var o = document.createStyleSheet(null, 0);
-        o.addRule('.dummy','display: static;');
+        o.addRule('.dummy', 'display: static;');
         o.cssText = 'html, head, head *, body, *.before, *.after, *.before *, *.after * { behavior: none; } * { behavior: expression($.pseudo(this)); }';
     }
-
 })(jQuery);
