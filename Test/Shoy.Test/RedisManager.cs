2f82e41f6ab196e979f7015d68933ded50512ed9
shoy <634330628@qq.com>
2015-07-27 17:42:12 +0800

    添加对Redis缓存服务器的测试

---
+++ Test/Shoy.Test/RedisManager.cs
@@ -0,0 +1,41 @@
+﻿using ServiceStack.Redis;
+using Shoy.Utility;
+
+namespace Shoy.Test
+{
+    public class RedisManager
+    {
+        private readonly PooledRedisClientManager _manager;
+
+        private RedisManager()
+        {
+            var config = new RedisClientManagerConfig
+            {
+                MaxWritePoolSize = 5,
+                MaxReadPoolSize = 5,
+                AutoStart = true
+            };
+            _manager = new PooledRedisClientManager(
+                new[] { "deyi@168@192.168.10.20:6379" },
+                new[] { "deyi@123@192.168.10.20:6380", "deyi@123@192.168.10.20:6381" }, config);
+//            _manager = new PooledRedisClientManager(
+//                new[] { "954f12aab80205ec@211.149.229.128:6379" },
+//                new[] { "862c53a755e16cc@211.149.229.128:6380", "40ca4419f50407cc@211.149.229.128:6381" }, config);
+        }
+
+        public static RedisManager Instance
+        {
+            get { return Singleton<RedisManager>.Instance ?? (Singleton<RedisManager>.Instance = new RedisManager()); }
+        }
+
+        public IRedisClient GetClient()
+        {
+            return _manager.GetClient();
+        }
+
+        public IRedisClient ReadOnlyClient()
+        {
+            return _manager.GetReadOnlyClient();
+        }
+    }
+}
---
+++ Test/Shoy.Test/RedisTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shoy.Utility;
+using Shoy.Utility.Extend;
+using StackExchange.Redis;
+
+namespace Shoy.Test
+{
+    [TestClass]
+    public class RedisTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var c = ConnectionMultiplexer.Connect(new ConfigurationOptions
+            {
+                Ssl = false,
+                Password = "deyi@168",
+                EndPoints = { { "127.0.0.1", 6379 } }
+            });
+            var shoy = c.GetDatabase().StringGet("shoy");
+            Console.WriteLine(shoy);
+        }
+
+        [TestMethod]
+        public void Test01()
+        {
+            const string key = "shoy";
+            var client = RedisManager.Instance.GetClient();
+            //            Console.WriteLine(client.Get<string>(key));
+            //            client.Remove(key);
+            client.Set(key, new DResult("测试阶段003"), TimeSpan.FromMinutes(20));
+            var result = client.Get<DResult>(key);
+            Console.WriteLine(result.ToJson());
+        }
+    }
+}
