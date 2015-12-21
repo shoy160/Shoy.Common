69c7a4ac34396a272ef133e2378450b4aaec9013
shoy <634330628@qq.com>
2015-12-21 17:20:12 +0800

    调整Application日志级别

--- Framework/Shoy.Web/DApplication.cs
+++ Framework/Shoy.Web/DApplication.cs
@@ -46,26 +46,26 @@ namespace Shoy.Web
 
         protected virtual void Session_Start(object sender, EventArgs e)
         {
-            _logger.Info("Session_Start...");
+            _logger.Debug("Session_Start...");
         }
 
         protected virtual void Session_End(object sender, EventArgs e)
         {
-            _logger.Info("Session_End...");
+            _logger.Debug("Session_End...");
         }
         protected virtual void Application_BeginRequest(object sender, EventArgs e)
         {
-            _logger.Info("Application_BeginRequest...");
+            _logger.Debug("Application_BeginRequest...");
         }
 
         protected virtual void Application_EndRequest(object sender, EventArgs e)
         {
-            _logger.Info("Application_EndRequest...");
+            _logger.Debug("Application_EndRequest...");
         }
 
         protected virtual void Application_AuthenticateRequest(object sender, EventArgs e)
         {
-            _logger.Info("Application_AuthenticateRequest...");
+            _logger.Debug("Application_AuthenticateRequest...");
         }
 
         protected virtual void Application_Error(object sender, EventArgs e)
