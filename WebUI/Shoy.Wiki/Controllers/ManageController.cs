37356bb4522276ef274180c1e10e7bab54436ae5
shoy <634330628@qq.com>
2015-12-25 17:02:53 +0800

    wiki manage

---
+++ WebUI/Shoy.Wiki/Controllers/ManageController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Shoy.Web;
+using Shoy.Wiki.Models.Dtos;
+
+namespace Shoy.Wiki.Controllers
+{
+    [RoutePrefix("manage")]
+    public class ManageController : DController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult WikiList()
+        {
+            var list = new List<WikiDto>();
+            return View(list);
+        }
+    }
+}
